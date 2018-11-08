d6c268cd85ad470b91cae3dbfaa95e0372bf680f
Sayori Arstryle <pw.td.gk@gmail.com>
2018-11-07 20:23:09 -0500

    reorder promises in test js to properly load info

--- frontend/postauth.js
+++ frontend/postauth.js
@@ -47,13 +47,12 @@ document.addEventListener('DOMContentLoaded', () => {
 fetchToken().then(response => {
     const regex = /(?<=<eBayAuthToken>)(.*)(?=<\/eBayAuthToken>)/gm
     setCookie("eBayAuthToken", regex.exec(response.data)[0], 1)
+    document.querySelector("#auth-token").innerHTML = getCookie("eBayAuthToken")
+    confirmIdentity().then(response => {
+        const regex = /(?<=<UserID>)(.*)(?=<\/UserID>)/gm
+        setCookie("userID", regex.exec(response.data)[0], 1)
+        document.querySelector("#auth-name").innerHTML = getCookie("userID")
+    })
 })
-confirmIdentity().then(response => {
-    const regex = /(?<=<UserID>)(.*)(?=<\/UserID>)/gm
-    setCookie("userID", regex.exec(response.data)[0], 1)
-})
-document.querySelector("#auth-name").innerHTML = getCookie("userID")
 document.querySelector("#auth-sessionid").innerHTML = getCookie("SessionID")
-document.querySelector("#auth-token").innerHTML = getCookie("eBayAuthToken")
-
 })
