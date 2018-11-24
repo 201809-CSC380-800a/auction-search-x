97e3dc0cb9cd5a36c84be753840fa53410433273
Prappe <pw.td.gk@gmail.com>
2018-11-24 22:40:42 +0000

    different api link

--- frontend/signin-auth.js
+++ frontend/signin-auth.js
@@ -2,7 +2,7 @@ function getSessionID() {
     return eBayRequest("GetSessionID", {
             sendSessionID: false,
             sendCredentials: false,
-            extraXML: "<RuName>Anthony_Levine-AnthonyL-Auctio-plpgzby</RuName>"
+            extraXML: "<RuName>Anthony_Levine-AnthonyL-Auctio-xbaca</RuName>"
         })
 }
 
@@ -10,6 +10,6 @@ function redirectToLogin() {
     getSessionID().then(response => {
         const regex = /(?<=<SessionID>)(.*)(?=<\/SessionID>)/gm
         setCookie("SessionID", regex.exec(response.data)[0], 1)
-        window.location.href = `https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&runame=Anthony_Levine-AnthonyL-Auctio-plpgzby&SessID=${getCookie("SessionID")}`
+        window.location.href = `https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&runame=Anthony_Levine-AnthonyL-Auctio-xbaca&SessID=${getCookie("SessionID")}`
     })
 }
