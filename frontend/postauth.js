e346a2fcebd82ef7da4d370f40a265e6b3e19f38
Prappe <pw.td.gk@gmail.com>
2018-12-03 10:12:36 +0000

    modify search to use Buy API instead of... page scrubbing... lmao

--- frontend/postauth.js
+++ frontend/postauth.js
@@ -7,6 +7,15 @@ function confirmIdentity() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+axios.post('https://sayori.xyz/api_j/identity/v1/oauth2/token?grant_type=client_credentials&redirect_uri=Anthony_Levine-AnthonyL-Auctio-xbaca&scope=https://api.ebay.com/oauth/api_scope', 
+    null, {
+        headers:{
+        'Authorization': `Basic ${window.btoa('AnthonyL-Auctionx-SBX-a7f4c67ed-79b83e10:SBX-7f4c67edd28f-d05b-4ac0-89c4-ad90')}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+.then(r => setCookie('access_token', r.data.access_token, 1))
+.catch(e => {throw e})
 document.querySelector('#auth-fullname').innerHTML = getCookie('user-firstname') + ' ' + getCookie('user-lastname')
 fetchToken().then(response => {
     const regex = /(?<=<eBayAuthToken>)(.*)(?=<\/eBayAuthToken>)/gm
@@ -17,7 +26,12 @@ fetchToken().then(response => {
         setCookie("userID", regex.exec(response.data)[0], 1)
         document.querySelector("#auth-name").innerHTML = getCookie("userID")
     })
+    .catch(e => {throw e})
 })
+.catch(e => {throw e})
+
+
+
 document.querySelector("#auth-sessionid").innerHTML = getCookie("SessionID")
 })
 
